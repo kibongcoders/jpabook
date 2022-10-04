5b89d414b60ec8063bdf3a39fbdfb58055cb97ee
Bong <kkp02052@gmail.com>
2022-10-04 22:49:58 +0900

    Initial commit

---
+++ src/main/java/kibong/jpabook/JpabookApplication.java
@@ -0,0 +1,13 @@
+package kibong.jpabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpabookApplication.class, args);
+	}
+
+}
